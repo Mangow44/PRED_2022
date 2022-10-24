ec6e8ed711b123accad59f62902ccf3b5f4919cb
Thomas Clouet <thomas.clouet1@etu.univ-nantes.fr>
2022-10-24 16:36:56 +0200

    :sparkles: Externals links

--- src/lib/navbar/Menu.svelte
+++ src/lib/navbar/Menu.svelte
@@ -1,14 +1,25 @@
 <script>
+	import { goto } from '$app/navigation';
+
 	export let showMenu = false;
 </script>
 
 <div
 	id="menu"
-	class="fixed top-[5rem] w-full h-[calc(100vh-5rem)] 
+	class="fixed flex flex-col top-[5rem] w-full h-[calc(100vh-5rem)] 
 		bg-c-l-green transitionMenu z-20
 		{showMenu ? 'showMenu' : 'defaultPositionXMenu'}"
 >
-	TODO
+	<button
+		class="w-[90%] h-[4rem] mx-auto mt-[1rem]
+			text-2xl font-bold rounded-xl shadow-lg bg-white
+			transition-all hover:scale-105"
+		on:click={() => {
+			goto('/liens_externes');
+		}}
+	>
+		Liens externes
+	</button>
 </div>
 
 <style>
